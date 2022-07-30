41d2415aa53c344b124d51a65189a4bd7611c5e6
JeonInha <104603885+JeonInha@users.noreply.github.com>
2022-07-30 21:25:30 +0900

    Update MultiEndingJudge.java

--- Game_proto/src/savehere/MultiEndingJudge.java
+++ Game_proto/src/savehere/MultiEndingJudge.java
@@ -5,51 +5,64 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MultiEndingJudge {
-	// 노말 엔딩 조건
-	List<Integer> normalEndingC1 = Arrays.asList(2, 3);
+	static final int REAL_ENDING = 1;
+	static final int NORMAL_ENDING = 2;
+	static final int BAD_ENDING = 3;
 	
 	// 배드엔딩 조건
-	List<Integer> BadEndingC1 = Arrays.asList(1, 4);
+	List<Integer> badC1 = Arrays.asList(1, 4);
+	List<Integer> badC2 = Arrays.asList(1, 4);
 	
 	// 리얼엔딩 조건
 	List<Integer> realC1 = Arrays.asList(6, 5);
-	
-	// 유저 정보::
-	List<Integer> userC1;
-	List<Integer> userC2;
+	List<Integer> realC2 = Arrays.asList(6, 5);
 	
 	
 	List<Integer> getStrToInt(String arr) {
 		List<Integer> arr2 = new ArrayList<>();
 		
 		arr = arr.replace(" ", "").replace("[", "").replace("]", "");
-        String[] saveArr2 = arr.split(",");
+        	String[] saveArr2 = arr.split(",");
         
-        for (String a : saveArr2) {
-        	arr2.add(Integer.valueOf(a));
-        }
+        	for (String a : saveArr2) {
+        		arr2.add(Integer.valueOf(a));
+       		}
 		return arr2;
 	}
 	
-	List<Integer> getUserC1Path(SaveHere user) {
-		String storyPath1 = user.getStroyPath1();
-		List<Integer> arr = getStrToInt(storyPath1);
-		
-		if (arr.contains(realC1)) {
-			System.out.println("real");
-		} else if (arr.contains(normalEndingC1)) {
-			System.out.println("노멀");
-		} else if (arr.contains(BadEndingC1)) {
-			System.out.println("배드");
+	boolean isContain(List<Integer> userPath, List<Integer> endingPath) {
+		int count = 0;
+		for (int i=0; i<endingPath.size(); i++) {	
+			if (userPath.contains(endingPath.get(i)))
+				count++;
 		}
-		
-		return arr;
+		if (count == endingPath.size()) {
+			return true;
+		}
+		return false;
 	}
 	
-	List<Integer> getUserC2Path(SaveHere user) {
-		List<Integer> arr = new ArrayList<>();
-		return arr;
+	int getEndingNum(SaveHere user) {
+		String storyPath1 = user.getStoryPath1();
+		String storyPath2 = user.getStoryPath2();
+		
+		List<Integer> arr = getStrToInt(storyPath1);
+		List<Integer> arr2 = getStrToInt(storyPath2);
+		
+		if (isContain(arr, realC1)) {
+			if (isContain(arr2, realC2)) {
+			ending = REAL_ENDING;
+			}
+		} else if (isContain(arr, badC1)) {
+			if (isContain(arr2, badC2)) {
+				ending = BAD_ENDING;
+			}
+		} else {
+			ending = NORMAL_ENDING;
+		} 
+		return ending;
 	}
+	
 	
 	public static void main(String[] args) {
 		MultiEndingJudge a = new MultiEndingJudge();
